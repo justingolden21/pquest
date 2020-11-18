26ae4c757f12d47d31f5aed6785b328762e73af5
RGB Studios <justingolden21@gmail.com>
2020-11-18 15:45:47 -0800

    init

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $( ()=> {
+
+	// generate faces
 	let html = '';
 	for(let i=0; i<151; i++) {
 		html += `
@@ -8,6 +10,48 @@ $( ()=> {
 		</div>`;
 	}
 	$('#faces').append(html);
+
+
+
+
+	// todo: make it so clicking toggle changes value of checkbox
+	$('.toggle__element').click( (event)=> {
+		// $(event.target).val($(event.target).val() == 'on' ? 'off' : 'on');
+		console.log($('#dex-toggle').is(':checked') );
+
+		$('#dex-toggle').attr('checked', true);
+		$('#dex-toggle').prop('checked', true);
+
+		// console.log($(event.target).find('.toggle__checkbox').length );
+		// console.log($(event.target).children('.toggle__checkbox').length );
+		// console.log($(event.target).children().find('.toggle__checkbox').length );
+		// $(event.target).children('.toggle__checkbox').click();
+
+		console.log($('#dex-toggle').is(':checked') );
+	});
+	// const TOGGLE_PARTS = 'label element checkbox line dot text'.split(' ');
+	// for( part of TOGGLE_PARTS) {
+	// 	$(`.toggle__${part}`).click( ()=> {
+	// 		console.log('toggle thing clicked');
+	// 		$(this).find('toggle__element').focus();
+	// 	});
+	// }
+
+	// make enter and space work on focused toggles
+	$(document).on('keydown', function (event) {
+		if( (event.which == 13 || event.which == 32) && $(event.target).hasClass('toggle__element') ) { // 13 and 32 enter and space
+			event.preventDefault();
+			$(event.target).click();
+		}
+	});
+
+	// clicking toggle text focuses toggle
+	$('.toggle__text').click( (event)=> {
+		$(event.target).siblings()[0].focus();
+	});
+
+
+
 
 });
 
